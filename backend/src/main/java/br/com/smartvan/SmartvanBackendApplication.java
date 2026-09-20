8adf599ff1bec93538e005a7e2fb73ea601c92b7
Guto <guto050506@gmail.com>
2026-09-20 12:17:10 -0300

    [CHORE] Configuração da estrutura inicial do projeto e backend Spring Boot

---
+++ backend/src/main/java/br/com/smartvan/SmartvanBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartvanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartvanBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/smartvan/SmartvanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartvanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
